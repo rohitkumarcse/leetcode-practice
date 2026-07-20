5f3bddeb5cb2f50b1fe742213d3a08be5478a434
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-07-20 11:04:41 +0530

    Time: 10 ms (11.72%) | Memory: 46.8 MB (96.55%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int total = n * m;
+        k = k % total;
+
+        for (int r = 0; r < k; r++) {
+
+            int last = grid[n - 1][m - 1];
+
+            // Shift every element one position to the right
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = m - 1; j >= 0; j--) {
+
+                    if (i == 0 && j == 0) {
+                        grid[i][j] = last;
+                    } else if (j == 0) {
+                        grid[i][j] = grid[i - 1][m - 1];
+                    } else {
+                        grid[i][j] = grid[i][j - 1];
+                    }
+                }
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int[] row : grid) {
+            List<Integer> list = new ArrayList<>();
+
+            for (int val : row) {
+                list.add(val);
+            }
+
+            ans.add(list);
+        }
+
+        return ans;
+    }
+}
