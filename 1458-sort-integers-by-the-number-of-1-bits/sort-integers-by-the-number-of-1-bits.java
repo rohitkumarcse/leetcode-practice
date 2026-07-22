2118beb3ec2ec5e4905a7dd67dc03a6e3c90c3fd
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-07-22 15:58:38 +0530

    Time: 11 ms (27.88%) | Memory: 46.9 MB (17.61%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -39,15 +39,50 @@
 // }
 
 // -------------------------------------------- Approach -2 -------------------
+// class Solution {
+//     public int[] sortByBits(int[] arr) {
+//         int[] bin = new int[10001];
+
+//         for (int i = 1; i < 10001; i++) {
+//             if (i % 2 == 0) {
+//                 bin[i] = bin[i /2];
+//             } else {
+//                 bin[i] = bin[i /2 ] + 1;
+//             }
+//         }
+
+//         Integer[] nums = new Integer[arr.length];
+
+//         for (int i = 0; i < arr.length; i++) {
+//             nums[i] = arr[i];
+//         }
+
+//         Arrays.sort(nums, (a, b) -> {
+//             if (bin[a] == bin[b]) {
+//                 return a - b;
+//             }
+
+//             return bin[a] - bin[b];
+//         });
+
+//         for (int i = 0; i < arr.length; i++) {
+//             arr[i] = nums[i];
+//         }
+
+//         return arr;
+//     }
+// }
+
+// -------------------------------------------- Approach -3 -------------------
 class Solution {
     public int[] sortByBits(int[] arr) {
         int[] bin = new int[10001];
 
         for (int i = 1; i < 10001; i++) {
             if (i % 2 == 0) {
-                bin[i] = bin[i / 2];
+                bin[i] = bin[i >> 1];
             } else {
-                bin[i] = bin[i / 2] + 1;
+                bin[i] = bin[i >> 1] + 1;
             }
         }
 
