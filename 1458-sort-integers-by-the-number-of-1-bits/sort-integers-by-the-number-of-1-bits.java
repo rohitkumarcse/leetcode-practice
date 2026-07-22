e318901fe4028cbfa042f3e306772a8fe918d2a7
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-07-22 15:56:25 +0530

    Time: 11 ms (27.88%) | Memory: 46.7 MB (29.87%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,37 +1,74 @@
+//------------------------------------------- Approach -1 -------------------------------------
+
+// class Solution {
+//     public int[] sortByBits(int[] arr) {
+//         int n = arr.length;
+//         int[][] res = new int[n][2];
+
+//         for (int i = 0; i < arr.length; i++) {
+//             res[i][0] = arr[i];
+//             res[i][1] = countOnes(arr[i]);
+//         }
+
+//         Arrays.sort(res, (a, b) -> {
+//             if (a[1] == b[1]) {
+//                 return a[0] - b[0];
+//             }
+//             return a[1] - b[1];
+//         });
+
+//         int[] ans = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             ans[i] = res[i][0];
+//         }
+
+//         return ans;
+//     }
+
+//     public int countOnes(int n) {
+//         int count = 0;
+
+//         while (n != 0) {
+//             n = n & (n - 1);
+//             count++;
+//         }
+
+//         return count;
+//     }
+// }
+
+// -------------------------------------------- Approach -2 -------------------
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int n = arr.length;
-        int[][] res = new int[n][2];
+        int[] bin = new int[10001];
+
+        for (int i = 1; i < 10001; i++) {
+            if (i % 2 == 0) {
+                bin[i] = bin[i / 2];
+            } else {
+                bin[i] = bin[i / 2] + 1;
+            }
+        }
+
+        Integer[] nums = new Integer[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            res[i][0] = arr[i];
-            res[i][1] = countOnes(arr[i]);
+            nums[i] = arr[i];
         }
 
-        Arrays.sort(res, (a, b) -> {
-            if (a[1] == b[1]) {
-                return a[0] - b[0];
+        Arrays.sort(nums, (a, b) -> {
+            if (bin[a] == bin[b]) {
+                return a - b;
             }
-            return a[1] - b[1];
+
+            return bin[a] - bin[b];
         });
 
-        int[] ans = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            ans[i] = res[i][0];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = nums[i];
         }
 
-        return ans;
-    }
-
-    public int countOnes(int n) {
-        int count = 0;
-
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-
-        return count;
+        return arr;
     }
 }
