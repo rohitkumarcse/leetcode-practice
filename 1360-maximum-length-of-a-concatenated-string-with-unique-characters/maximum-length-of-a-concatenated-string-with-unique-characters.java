7ea48b8990fd33b03614f1d066a4f43bb5661931
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-07-27 13:03:02 +0530

    Time: 2 ms (98.86%) | Memory: 42.6 MB (96.58%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,3 +1,56 @@
+// class Solution {
+//     static int result;
+
+//     public int maxLength(List<String> arr) {
+//         result = 0;
+
+//         List<Integer> uniqueStrings = new ArrayList<>();
+
+//         for (String str : arr) {
+//             Set<Character> set = new HashSet<>();
+
+//             for (char ch : str.toCharArray()) {
+//                 set.add(ch);
+//             }
+
+//             // Contains duplicate characters
+//             if (set.size() != str.length()) {
+//                 continue;
+//             }
+
+//             int val = 0;
+
+//             for (char ch : str.toCharArray()) {
+//                 val = val | (1 << (ch - 'a'));
+//             }
+
+//             uniqueStrings.add(val);
+//         }
+
+//         int temp = 0;
+
+//         solve(0, temp, uniqueStrings);
+
+//         return result;
+//     }
+
+//     public static void solve(int idx, int temp, List<Integer> uniqueStrings) {
+//         result = Math.max(result, Integer.bitCount(temp));
+
+//         for (int i = idx; i < uniqueStrings.size(); i++) {
+//             if ((temp & uniqueStrings.get(i)) == 0) {
+//                 solve(
+//                     i + 1,
+//                     temp | uniqueStrings.get(i),
+//                     uniqueStrings
+//                 );
+//             }
+//         }
+//     }
+// }
+
+
+// approach 2 remove hashset dependecy 
 class Solution {
     static int result;
 
@@ -7,34 +60,37 @@ class Solution {
         List<Integer> uniqueStrings = new ArrayList<>();
 
         for (String str : arr) {
-            Set<Character> set = new HashSet<>();
-
-            for (char ch : str.toCharArray()) {
-                set.add(ch);
-            }
-
-            // Contains duplicate characters
-            if (set.size() != str.length()) {
-                continue;
-            }
-
             int val = 0;
+            boolean valid = true;
 
             for (char ch : str.toCharArray()) {
-                val = val | (1 << (ch - 'a'));
+                int bit = 1 << (ch - 'a');
+
+                // Character already exists
+                if ((val & bit) != 0) {
+                    valid = false;
+                    break;
+                }
+
+                // Add character to bitmask
+                val |= bit;
             }
 
-            uniqueStrings.add(val);
+            if (valid) {
+                uniqueStrings.add(val);
+            }
         }
 
-        int temp = 0;
-
-        solve(0, temp, uniqueStrings);
+        solve(0, 0, uniqueStrings);
 
         return result;
     }
 
-    public static void solve(int idx, int temp, List<Integer> uniqueStrings) {
+    public static void solve(
+        int idx,
+        int temp,
+        List<Integer> uniqueStrings
+    ) {
         result = Math.max(result, Integer.bitCount(temp));
 
         for (int i = idx; i < uniqueStrings.size(); i++) {
