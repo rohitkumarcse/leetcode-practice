17745dc3d3b0a207af9400f704a3d42a8790142d
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-07-29 21:51:06 +0530

    Time: 36 ms (24.40%) | Memory: 48.1 MB (76.00%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,18 +1,46 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
+// class Solution {
+//     public String smallestPalindrome(String s) {
+//         int n = s.length();
+//         int mid = n / 2;
+
+//         char[] arr = s.toCharArray();
+
+//         Arrays.sort(arr, 0, mid);
+
+//         for (int i = 0; i < mid; i++) {
+//             arr[n - 1 - i] = arr[i];
+//         }
+
+//         return new String(arr);
+//     }
+// }
+// ----------------------------------------------- Approach -2 -------------------
 class Solution {
     public String smallestPalindrome(String s) {
-        int n = s.length();
-        int mid = n / 2;
+        int[] freq = new int[26];
 
-        char[] arr = s.toCharArray();
-
-        Arrays.sort(arr, 0, mid);
-
-        for (int i = 0; i < mid; i++) {
-            arr[n - 1 - i] = arr[i];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
-        return new String(arr);
+        StringBuilder first = new StringBuilder();
+        StringBuilder mid = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] >= 2) {
+                first.append((char) ('a' + i));
+                freq[i] -= 2;
+            }
+
+            if (freq[i] == 1 && mid.length() == 0) {
+                mid.append((char) ('a' + i));
+            }
+        }
+
+        StringBuilder second = new StringBuilder(first).reverse();
+
+        return first.toString() + mid.toString() + second.toString();
     }
 }
