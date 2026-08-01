b93bbc0d8e6bc6649bb562eca65f8b576215ee9b
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-08-01 13:49:45 +0530

    Time: 32 ms (86.27%) | Memory: 71.6 MB (38.56%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,9 +1,44 @@
+// class Solution {
+//     public int largestCombination(int[] candidates) {
+
+//         int ans = 0;
+//         use maxbit = log2(10^7)
+
+//         for (int bit = 0; bit <= 31; bit++) {
+//             int cnt = 0;
+
+//             for (int i = 0; i < candidates.length; i++) {
+//                 if ((candidates[i] & (1 << bit)) != 0) {
+//                     cnt++;
+//                 }
+//             }
+
+//             ans = Math.max(ans, cnt);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int largestCombination(int[] candidates) {
 
         int ans = 0;
 
-        for (int bit = 0; bit <= 31; bit++) {
+        // Formula:
+        // maxBit = floor(log2(maxElement))
+        // Number of bits = floor(log2(maxElement)) + 1
+
+        int max = 0;
+        for (int x : candidates) {
+            max = Math.max(max, x);
+        }
+
+        int maxBit = (int) (Math.log(max) / Math.log(2));
+        // or: int maxBit = 31 - Integer.numberOfLeadingZeros(max);
+
+        for (int bit = 0; bit <= maxBit; bit++) {
             int cnt = 0;
 
             for (int i = 0; i < candidates.length; i++) {
