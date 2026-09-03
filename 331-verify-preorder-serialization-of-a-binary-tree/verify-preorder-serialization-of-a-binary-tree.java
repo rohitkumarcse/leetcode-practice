c3a981f885a014c4f86e9cdef818f570cd6de7ba
rohitkumarcse <85194536+rohitkumarcse@users.noreply.github.com>
2026-09-03 14:25:51 +0530

    Time: 4 ms (96.52%) | Memory: 44.8 MB (77.85%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
@@ -1,25 +1,50 @@
-class Solution { 
-    public boolean isValidSerialization(String preorder) { 
-        Stack<Integer> st = new Stack<>(); 
-        String tokens[] = preorder.split(","); 
+// class Solution { 
+//     public boolean isValidSerialization(String preorder) { 
+//         Stack<Integer> st = new Stack<>(); 
+//         String tokens[] = preorder.split(","); 
  
-        st.push(1); 
+//         st.push(1); 
  
-        for (int i = 0; i < tokens.length; i++) { 
-            String ch = tokens[i]; 
+//         for (int i = 0; i < tokens.length; i++) { 
+//             String ch = tokens[i]; 
  
-            if (st.isEmpty()) return false; 
+//             if (st.isEmpty()) return false; 
             
-            int slot = st.pop(); 
-            slot--; 
+//             int slot = st.pop(); 
+//             slot--; 
             
-            if (slot > 0) st.push(slot); 
+//             if (slot > 0) st.push(slot); 
             
-            if (!ch.equals("#")) { 
-                st.push(2); 
-            } 
-        } 
+//             if (!ch.equals("#")) { 
+//                 st.push(2); 
+//             } 
+//         } 
         
-        return st.isEmpty(); 
-    } 
+//         return st.isEmpty(); 
+//     } 
+// }
+
+
+//-------------------------------------------- approach -2 ----------------------------------- 
+
+
+class Solution {  
+    public boolean isValidSerialization(String preorder) {  
+        
+        String tokens[] = preorder.split(",");  
+  
+        int degree = 1;  
+  
+        for (int i = 0; i < tokens.length; i++) {  
+            String ch = tokens[i];  
+            if ( degree <= 0) return false;  
+                 degree--;         
+            
+            if (!ch.equals("#")) {  
+                degree += 2; 
+            }  
+        }  
+         
+        return degree == 0;   
+    }  
 }
